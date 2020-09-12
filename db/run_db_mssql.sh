29953c5c309f65bacce8bcada49a5f44e6c2cbb5
牛加人等于朱 <niuren.zhu@icloud.com>
2020-09-12 15:33:45 +0800

    修改docker设置

--- db/run_db_mssql.sh
+++ db/run_db_mssql.sh
@@ -14,27 +14,21 @@ echo '**************************************************************************
 WORK_FOLDER=$PWD
 NAME=ibas-db-mssql
 PORT=1433
-MEM=4g
-FOLDER=${WORK_FOLDER}/mssql
+MEM=2g
 PASSWD=Aa123456
 
-# 检查环境
-if [ ! -e "${FOLDER}" ];then mkdir -p "${FOLDER}"; fi;
 # 显示容器信息
 echo --容器名称：${NAME}
 echo --映射端口：${PORT}
 echo --限制内存：${MEM}
-echo --数据目录：${FOLDER}
 echo --用户密码：${PASSWD}
 
-docker start ${NAME} \
-|| \
-docker run \
-   --name ${NAME} \
-   -m ${MEM} \
-   -p ${PORT}:1433 \
-   -v ${FOLDER}:/var/opt/mssql \
-   -e ACCEPT_EULA=Y \
-   -e MSSQL_SA_PASSWORD=${PASSWD} \
-   -e MSSQL_PID=Developer \
-   -d microsoft/mssql-server-linux
+docker start ${NAME} ||
+   docker run \
+      --name ${NAME} \
+      -m ${MEM} \
+      -p ${PORT}:1433 \
+      -e ACCEPT_EULA=Y \
+      -e MSSQL_SA_PASSWORD=${PASSWD} \
+      -e MSSQL_PID=Developer \
+      -d microsoft/mssql-server-linux
--- db/run_db_pgsql.sh
+++ db/run_db_pgsql.sh
@@ -10,25 +10,19 @@ echo '**************************************************************************
 WORK_FOLDER=$PWD
 NAME=ibas-db-pgsql
 PORT=5432
-MEM=1g
-FOLDER=${WORK_FOLDER}/pgsql
+MEM=512m
 PASSWD=1q2w3e
 
-# 检查环境
-if [ ! -e "${FOLDER}" ];then mkdir -p "${FOLDER}"; fi;
 # 显示容器信息
 echo --容器名称：${NAME}
 echo --映射端口：${PORT}
 echo --限制内存：${MEM}
-echo --数据目录：${FOLDER}
 echo --用户密码：${PASSWD}
 
-docker start ${NAME} \
-|| \
-docker run \
-  --name ${NAME} \
-  -p ${PORT}:5432 \
-  -m ${MEM} \
-  -v ${FOLDER}:/var/lib/postgresql/data \
-  -e POSTGRES_PASSWORD=${PASSWD} \
-  -d postgres:alpine
+docker start ${NAME} ||
+  docker run \
+    --name ${NAME} \
+    -p ${PORT}:5432 \
+    -m ${MEM} \
+    -e POSTGRES_PASSWORD=${PASSWD} \
+    -d postgres:alpine
